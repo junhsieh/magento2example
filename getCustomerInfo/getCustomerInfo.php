ba767a9681a41192ed6aacdb503fd4e4a18ed3a6
Jun Hsieh <jun.github@ijun.org>
2017-02-12 17:52:15 -0800

    Using common.php

--- getCustomerInfo/getCustomerInfo.php
+++ getCustomerInfo/getCustomerInfo.php
@@ -1,17 +1,9 @@
 <?php
-use \Magento\Framework\App\Bootstrap;
+include __DIR__ . '/../common.php';
 
-include('/www/magento2.1/app/bootstrap.php');
-
-$bootstrap = Bootstrap::create(BP, $_SERVER);
-
-$objectManager = $bootstrap->getObjectManager();
-
-$storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-
-$state = $objectManager->get('\Magento\Framework\App\State');
 $state->setAreaCode('base');
 
+$storeManager = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
 $storeId = $storeManager->getStore()->getId();
 
 #$websiteId = $storeManager->getWebsite()->getWebsiteId();
