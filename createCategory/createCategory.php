f7d343bd35a1207a41f90a976dfab240b843cc6e
Jun Hsieh <jun.github@ijun.org>
2017-02-13 16:38:30 -0800

    Add some more info

--- createCategory/createCategory.php
+++ createCategory/createCategory.php
@@ -3,7 +3,7 @@ include __DIR__ . '/../common.php';
 
 $state->setAreaCode('base');
 
-createCategory();
+#createCategory();
 listCategory();
 
 function createCategory() {
@@ -28,12 +28,18 @@ function listCategory() {
 
 	foreach ($categoryCollection as $category) {
 		#print_r($category->getData());
+		#print_r($category->getCustomAttributes());
 
 		echo 'Id: ' . $category->getId() . PHP_EOL;
 		echo 'Name: ' . $category->getName() . PHP_EOL;
 		echo 'ParentId: ' . $category->getParentId() . PHP_EOL;
 		echo 'Url: ' . $category->getUrl() . PHP_EOL;
 		echo 'UrlKey: ' . $category->getUrlKey() . PHP_EOL;
+		echo 'Path: ' . $category->getPath() . PHP_EOL;
+		echo 'Position: ' . $category->getPosition() . PHP_EOL;
+		echo 'Level: ' . $category->getLevel() . PHP_EOL;
+		echo 'ChildrenCount: ' . $category->getChildrenCount() . PHP_EOL;
+		echo 'Description: ' . $category->getDescription() . PHP_EOL;
 		echo PHP_EOL;
 	}
 }
