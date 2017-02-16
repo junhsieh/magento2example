b335555bd067af38c4244f0ab4f07bd802454a9e
Jun Hsieh <jun.github@ijun.org>
2017-02-15 16:13:41 -0800

    Set IsInStock

--- updateStockInfo/updateStockInfo.php
+++ updateStockInfo/updateStockInfo.php
@@ -5,8 +5,8 @@ $state->setAreaCode('base');
 
 ### Reference:
 ### http://magento.stackexchange.com/questions/96519/magento-2-programmatically-updating-inventory
-$sku = 'WWW1';
-$stockQty = 55;
+$sku = 'MP22';
+$stockQty = 54;
 
 $productRepository = $objectManager->create('\Magento\Catalog\Api\ProductRepositoryInterface');
 $stockRegistry = $objectManager->create('\Magento\CatalogInventory\Api\StockRegistryInterface');
@@ -18,8 +18,12 @@ $product = $productRepository->get($sku);
 $stockItem = $stockRegistry->getStockItem($product->getId());
 
 $stockItem->setQty($stockQty);
-#$stockItem->setData('qty', $stockQty);
 
+if ($stockQty > 0) {
+	$stockItem->setIsInStock(true);
+}
+
+#$stockItem->setData('qty', $stockQty);
 #$stockItem->setData('manage_stock', $stockData['manage_stock']);
 #$stockItem->setData('is_in_stock', $stockData['is_in_stock']);
 #$stockItem->setData('use_config_notify_stock_qty', 1);
